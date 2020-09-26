1423282530778faa736b8618f9df90c6e2941853
WillianFarias <williansousafarias@gmail.com>
2020-09-26 12:42:11 -0300

    spread

--- main.js
+++ main.js
@@ -167,3 +167,19 @@ function somaRest(...params) {
 console.log(somaRest(1, 3, 4));
 
 //SPREAD
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+const arr3 = [...arr1, ...arr2];
+
+console.log(arr3);
+
+const usuario1 = {
+  nome: 'Diego',
+  idade: 23,
+  empresa: 'Rocketseat'
+};
+
+const usuario2 = { ...usuario1, nome: 'Willian'};
+
+console.log(usuario2);
