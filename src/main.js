5ee0509a97d0d10b8877b31fb109ffe71b56a515
WillianFarias <williansousafarias@gmail.com>
2020-09-29 11:25:44 -0300

    axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-const minhaPromise = () => new Promise((resolve, reject) => {
+/*const minhaPromise = () => new Promise((resolve, reject) => {
   setTimeout(() => { resolve('OK')}, 2000);
 });
 
-/*
+
 minhaPromise()
   .then(response => {
     console.log(response);
@@ -10,7 +10,7 @@ minhaPromise()
   .catch(err ={
 
   });
-  */
+
 
   //await só pode ser executado dentro de uma async function
   async function executaPromise() {
@@ -19,10 +19,24 @@ minhaPromise()
     console.log(await minhaPromise());
   }
 
-  /*
   const executaPromise = async () => {
     console.log(await minhaPromise());
   }
-  */
 
   executaPromise();
+  */
+ import axios from 'axios';
+
+ class Api {
+   static async getUserInfo(username) {
+     try {
+      const response = await axios.get(`https://api.github.com/users/${username}`);
+      console.log(response);
+     } catch (error) {
+       console.warn('Erro na API');
+     }
+   }
+ }
+
+ Api.getUserInfo('WillianFarias');
+ Api.getUserInfo('WillianFariaasdasdasds');
