4a598f215ab40f3191ac43fb4a2a6317556d06a7
WillianFarias <williansousafarias@gmail.com>
2020-09-29 00:39:59 -0300

    Webpack dev server

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 //import soma, { sub } from './functions';
 //import somaFunction from './soma'
-import * as funcoes from './functions';
+/*import * as funcoes from './functions';
 
 console.log(funcoes.soma(1, 2));
-console.log(funcoes.sub(1, 2));
+console.log(funcoes.sub(1, 2));*/
+
+alert('Testando');
