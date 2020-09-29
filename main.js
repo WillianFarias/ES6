4fd4fac413e573c8e4ff178daa9b69a4ea4848c8
WillianFarias <williansousafarias@gmail.com>
2020-09-29 00:29:26 -0300

    import/export

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-import { soma } from './functions';
+//import soma, { sub } from './functions';
+//import somaFunction from './soma'
+import * as funcoes from './functions';
 
-console.log(soma(1, 2));
+console.log(funcoes.soma(1, 2));
+console.log(funcoes.sub(1, 2));
